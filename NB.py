eefb6f54dc2f1d3682803b82f081aaeac851291b
pallavi24 <pallavikotkar17@gmail.com>
2016-03-09 19:24:53 -0800

    Probabilities to the CSV

--- NB.py
+++ NB.py
@@ -37,9 +37,6 @@ def convertToFeatures(train):
     train = applyFunction(train, 'PdDistrict',"TENDERLOIN", "TENDERLOIN")
     return train
 
-
-
-
 #Subsetting data
 def sliceByCategory(categories, train):
     trainWithCategories = train.loc[train['Category'].isin(categories)]
@@ -66,8 +63,8 @@ def NaiveBayesClassifier(train, evaluate, test):
                'CENTRAL', 'INGLESIDE', 'MISSION', 'NORTHERN', 'PARK', 'RICHMOND', 'SOUTHERN', 'TARAVAL', 'TENDERLOIN']]
     #print(x[])
     y = train['Category'].astype('category')
-    #print(pd.unique(y.ravel()))
-    #all_categories =pd.unique(y.ravel())
+    #print(pd.unique(x.Category.ravel()))
+    #all_categories =pd.unique(x.Category.ravel())
     actual = evaluate['Category'].astype('category')
     classifier = GaussianNB()
 
@@ -77,19 +74,54 @@ def NaiveBayesClassifier(train, evaluate, test):
     classifier.fit(x,y)#.predict(evaluate[['X', 'Y', 'DayOfWeek']])
     print(classifier)
     y_pred = classifier.predict(x_test)
+
+    #Predicting probabilities for the categories
     y_prob = classifier.predict_proba(x_test)
+
+    print ("Sizes" + str(len(y_pred)) + " " + str(len(y_prob)) + str(y_pred[1]))
+
+    #Creating Dataframe
+    cols = ['WARRANTS', 'OTHER OFFENSES', 'LARCENY/THEFT', 'VEHICLE THEFT', 'VANDALISM', 'NON-CRIMINAL', 'ROBBERY',
+          'ASSAULT', 'WEAPON LAWS', 'BURGLARY', 'SUSPICIOUS OCC', 'DRUNKENNESS', 'FORGERY/COUNTERFEITING',
+          'DRUG/NARCOTIC', 'STOLEN PROPERTY', 'SECONDARY CODES', 'TRESPASS', 'MISSING PERSON', 'FRAUD', 'KIDNAPPING',
+          'RUNAWAY', 'DRIVING UNDER THE INFLUENCE', 'SEX OFFENSES FORCIBLE', 'PROSTITUTION', 'DISORDERLY CONDUCT',
+          'ARSON', 'FAMILY OFFENSES','LIQUOR', 'LAWS', 'BRIBERY', 'EMBEZZLEMENT', 'SUICIDE', 'LOITERING',
+          'SEX OFFENSES NON FORCIBLE', 'EXTORTION', 'GAMBLING', 'BAD CHECKS', 'TREA', 'RECOVERED VEHICLE',
+          'PORNOGRAPHY/OBSCENE MAT']
+
+    #Dataframe of probabilities
+    rows, col =  (y_prob.shape)
+    #print ('############ ROWS' + str(rows))
+    zero_data = np.zeros(shape=(rows, 40))
+    df = pd.DataFrame(zero_data,columns=cols)
+    print ('Dataframe created')
+    print (df[['BURGLARY']])
+    for index in range(0, rows):
+        df.loc[index,'LARCENY/THEFT'] = y_prob.item((index,0))
+        df.loc[index,'OTHER OFFENSES'] = y_prob.item((index,1))
+        df.loc[index,'NON-CRIMINAL'] = y_prob.item((index,2))
+        df.loc[index,'ASSAULT'] = y_prob.item((index,3))
+        df.loc[index,'DRUG/NARCOTIC'] = y_prob.item((index,4))
+
+    print (df[['BURGLARY']])
+    df.to_csv('Naive.csv', sep=',')
+    np.savetxt("NB.csv", y_prob, delimiter=",")
+
+
+    print ('Type: ' + str(type(y_prob)))
     val = llfun(actual, y_pred)
     print('Log loss: ' + str(val))
 
     #On testing data
-    #x_test = test[['X', 'Y', 'mon', 'sun', 'tues', 'thur', 'fri', 'sat', 'BAYVIEW',
-    #          'CENTRAL', 'INGLESIDE', 'MISSION', 'NORTHERN', 'PARK', 'RICHMOND', 'SOUTHERN', 'TARAVAL', 'TENDERLOIN']]
-    #x_test = x_test.head(10)
+    x_test = test[['X', 'Y', 'mon', 'sun', 'tues', 'thur', 'fri', 'sat', 'BAYVIEW',
+              'CENTRAL', 'INGLESIDE', 'MISSION', 'NORTHERN', 'PARK', 'RICHMOND', 'SOUTHERN', 'TARAVAL', 'TENDERLOIN']]
+    x_test = x_test.head(10)
     #assert isinstance(x_test, object)
-    #test_outcomes = classifier.predict(x_test)
-    #submit = pd.DataFrame({'Id': test.Id.tolist()})
-    #for category in all_categories:
-    #    submit[category] = np.where(y_pred == category, 1, 0)
+
+    test_outcomes = classifier.predict(x_test)
+    submit = pd.DataFrame({'Id':test.Id.tolist()})
+    #for category in y.cat.categories:
+    #    submit[category] = np.where(test_outcomes == category, 1, 0)
 
     #submit.to_csv('NB_test.csv', index = False)
     return y_pred
